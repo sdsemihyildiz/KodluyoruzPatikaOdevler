5863bc4f2ec23178c0c013932c895e865b077436
SDSemoch <sdsemihyildiz@gmail.com>
2022-09-18 20:57:15 +0300

    Maaş

---
+++ src/MaasHesaplayici/Main.java
@@ -0,0 +1,9 @@
+package MaasHesaplayici;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Semih", 10000, 48, 1982);
+        e1.toString();
+    }
+
+}
