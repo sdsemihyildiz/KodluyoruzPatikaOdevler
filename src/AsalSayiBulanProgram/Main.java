c21e6c7c7283ba5dc66307f907b442a285060d21
SDSemoch <sdsemihyildiz@gmail.com>
2022-09-18 20:34:21 +0300

    Create Main.java

---
+++ src/AsalSayiBulanProgram/Main.java
@@ -0,0 +1,27 @@
+package AsalSayiBulanProgram;
+
+import java.util.Scanner;
+
+public class Main {
+    static String asal(int x, int trial) {
+        if (x == 2) {
+            return " asal";
+        }
+        if (x % trial == 0) {
+            asal(x, trial + 1);
+
+            return " asal degil.";
+        } else {
+            return " asal.";
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayi giriniz: ");
+        int x = scan.nextInt();
+
+        System.out.println(x + asal(x, 2));
+    }
+}
